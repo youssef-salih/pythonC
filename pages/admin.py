43da8cff82565b360c5e402be90f7fe51dba7d41
youssef-salih <d.youssefsalih@gmail.com>
2024-01-20 13:34:52 +0100

    tp03 ad

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ("title","updatedate")
+    ordering = ('title',)
+    search_fields = ("title",)
+
+
+admin.site.register(Page,PageAdmin)
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    title=models.CharField(max_length=60)
+    permalink=models.CharField(max_length=12,unique=True)
+    updatedate=models.DateTimeField(verbose_name="Last Update")
+    bodytext = models.TextField("page content",blank=True)
+
+    def __str__(self):
+        return self.title
